cf48df0d89a7ae9a6dc3eb674c20d0db27fd6f0b
Foia Oleksandr <foia.alexander@gmail.com>
2025-07-10 15:38:50 +0300

    Add hex numbers and font settings

--- +model/Options.m
+++ +model/Options.m
@@ -1,7 +1,16 @@
 classdef Options
     
     properties
-        complexity uint32 {mustBeInteger, mustBePositive} = 3;
+        complexity uint32 {mustBeInteger, mustBePositive} = 3
+        hexNumbers logical = false
+        hexLettersUpperCase logical = false
+        fontName char = 'Calibri'
+        fontSize double = 18
+        fontWeight char = 'normal'
+        fontAngle char = 'normal'
+        fontColor = [0.00,0.00,0.00]
+        backgroundFontColor = [1.00,1.00,1.00]
+        backgroundColor = [0.94,0.94,0.94]
     end
 
     methods
